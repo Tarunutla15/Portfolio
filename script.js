fb033e330b4925b91e61497bc313c602d3341dcf
Tarunutla15 <142660669+Tarunutla15@users.noreply.github.com>
2024-09-20 08:56:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a')
+
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+            })
+        }
+    })
+}
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        // Use EmailJS to send the form
+        emailjs.sendForm('service_cwijoob', 'template_s8w5k4v', this)
+            .then(function() {
+                // Hide the form
+                document.getElementById('contact-form').style.display = 'none';
+
+                // Show the success message
+                document.getElementById('success-message').style.display = 'block';
+            }, function(error) {
+                alert('Failed to send your message. Please try again.');
+                console.log('FAILED...', error);
+            });
+    });
+};
